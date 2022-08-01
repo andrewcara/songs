4387d310283f82b244a218628359feb9ae3ca65a
andrewcara <64450522+andrewcara@users.noreply.github.com>
2022-08-01 16:59:52 -0400

    Added the add songs to a playlist functionality

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -1,11 +1,9 @@
 const requests = require('request');
+const path = require('path');
 
 
 exports.createPlaylist = (req, res, next) => {
-    
-    
-    console.log(req.session.access_token);
-    console.log(req.session.bod_id);
+  
 
         var options = {
             url: `https://api.spotify.com/v1/users/${req.session.bod_id}/playlists`,
@@ -25,12 +23,35 @@ exports.createPlaylist = (req, res, next) => {
         requests.post(options, function(error, response, body) {
             
             body = JSON.parse(body);
+            const tempPlaylist = body.uri.split(":"); //example response - spotify:playlist:2d0gkKNytoyBS8Nx1tuCE0 the uri is returned in response to the post request
+            req.session.playlist_id = (tempPlaylist[2]); //adding the playlist to the session
+            res.render(path.join(__dirname, '../', 'views', 'created-playlist.html'), {playlist: req.session.playlist_id});
 
-            if (body.error.status === 403){
-                res.redirect('/refresh-token'); //need to add this endpoint to get a refresh token if the one we are using expires
-            }
+            // //if (body.error.status === 403){
+            //     res.redirect('/refresh-token'); //need to add this endpoint to get a refresh token if the one we are using expires
+            // }
         });
 
 
 }
-
+// adding songs to spotify playlist
+ 
+exports.addToPlaylist = (req,res,next) => {
+ 
+   var request = require('request');
+       var authOptions1 = {
+           url: `https://api.spotify.com/v1/playlists/${req.session.playlist_id}/tracks`,
+           body: JSON.stringify({
+             "uris": ['spotify:track:0cFlXjTxFdMGGPfLVpt3Wv'] //array of strings - this is a test one
+           }),
+           dataType:'json',
+           headers: {
+               'Authorization': 'Bearer ' + req.session.access_token, //cannot make any requests to the spotify api without access_token
+               'Content-Type': 'application/json',
+           }
+       };
+ 
+       request.post(authOptions1, function(error, response, body) {
+           console.log(body);
+       });
+}
